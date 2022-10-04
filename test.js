5e17995f6754f98e3a3f6badfd50a8d1a3257698
anhiao <anhiao2358@gmail.com>
2022-10-05 07:22:58 +0800

    update

--- test.js
+++
@@ -1,8 +0,0 @@
-import test from './index.js';
-(async ()=>{
-    const TEST = new test("https://drive.aneme.cc/OneDrive_CN","nio2358@");
-    await TEST.login();
-    await TEST.upload("./[Comicat&KissSub][SPY x FAMILY][10][1080P][GB][MP4].mp4","rssiddght2.mov","/")
-    // console.log(await TEST.getFileSize("./IMG_0353.MOV"));
-    // await TEST.mkdir("letsgo1","/")
-})();
