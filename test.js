ce59bcd930b8ee8ac90ec9a16c0bc52a8aa45fc7
AnHiAo <85613555+AnHiAo@users.noreply.github.com>
2022-10-05 04:48:53 +0800

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-import test from './index.js';
-(async ()=>{
-    const TEST = new test("https://drive.aneme.cc/OneDrive_CN","nio2358@");
-    await TEST.login();
-    await TEST.mkdir("letsgo1","/")
-})();
